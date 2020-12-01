a5ff80b745db52b8a4075303d16be68b518e8f68
gabicaruso <38561978+gabicaruso@users.noreply.github.com>
2020-12-01 20:41:07 -0300

    update url

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('get', views.tasks, name='get_tasks'),
-    path('add', views.add, name='add_task'),
-    path('del', views.delete, name='del_tasks'),
+    path('get', views.get_tasks, name='get_tasks'),
+    path('add', views.add_task, name='add_task'),
+    path('del', views.del_tasks, name='del_tasks'),
 ]
