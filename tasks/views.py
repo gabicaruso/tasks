fc78ca9b141e9077e39529f60e480633562a9d00
gabicaruso <38561978+gabicaruso@users.noreply.github.com>
2020-12-01 19:41:53 -0300

    update view

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-# Create your views here.
-
-
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
+
+@api_view(["GET"])
+def get_tasks(request):
+    all_tasks = Task.objects.all()
+    tasks_json = serializers.serialize("json", all_tasks)
+    return HttpResponse(tasks_json ,content_type="application/json")
+
+@api_view(["POST"])
+def add_task(request):
+    dataJson = JSONParser().parse(request)
+    serializer = TaskSerializer(data=dataJson)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, status=201)
+    return JsonResponse(serializer.errors, status=400)
+
+@api_view(["DELETE"])
+def del_tasks(request):
+    Task.objects.all().delete()
+    return Response({"message": "All tasks deleted!"})
